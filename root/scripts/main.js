947c98feeb4e427bf7101f9830f7e89976e1dc1f
PiotrAs <piotr.slobinski@gmail.com>
2018-08-28 21:55:08 +0200

    added js functionalities and footer bg

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    // adds class to navbar on scroll
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 200) {
@@ -8,12 +9,58 @@ $(function () {
             $("nav").css("background", "transparent");
             $("nav").removeClass("shadow");
         }
+    });
+    // smooth scrol to sections
+    $('li:nth-child(1)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-1').offset().top - 40
+        }, 1000);
+    });
+    $('li:nth-child(2)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-2').offset().top - 40
+        }, 1000);
+    });
+    $('li:nth-child(3)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-3').offset().top - 40
+        }, 1000);
+    });
+    $('li:nth-child(4)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-4').offset().top - 40
+        }, 1000);
+    });
+    $('li:nth-child(5)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-5').offset().top - 40
+        }, 1000);
+    });
+    $('li:nth-child(6)').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.sec-6').offset().top - 40
+        }, 1000);
+    });
+    // TOP button toggle on scroll
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 750) {
+            $('#TOP').addClass('visible')
+        } else {
+            $('#TOP').removeClass('visible')
+        }
+    });
+    $('#TOP').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
     })
-
-
-
-
-
-
 
 }); //Main function
